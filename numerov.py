42b2bcfc2720bb631e37b1aa7fd866810bd9d97b
Radovan Bast <bast@users.noreply.github.com>
2017-04-01 21:48:04 +0200

    introduce main function

--- numerov.py
+++ numerov.py
@@ -6,7 +6,12 @@ import numpy
 from optparse import OptionParser
 
 
-def run_numerov(potential_coef,
+def run_numerov(options,
+                reduced_mass_amu,
+                amu_to_au,
+                hartree_to_hz,
+                hartree_to_cm1,
+                potential_coef,
                 property_coef,
                 q_max):
 
@@ -108,159 +113,168 @@ def run_numerov(potential_coef,
     return q, psi_squared, energy, diff_hz, transition_frequency
 
 
-version = '1.1.0'
+def main():
+    version = '1.1.0'
 
-amu_to_au      = 1822.888479031408
-hartree_to_hz  = 6.579683920721e15
-hartree_to_cm1 = 2.194746313705e5
-
-
-usage = '''
-  example: ./%prog --file=CHFClBr_mode3_x2c_b3lyp'''
-
-parser = OptionParser(usage)
-
-parser.add_option('--file',
-                  type='string',
-                  action='store',
-                  default=None,
-                  help='File to parse',
-                  metavar='FILE')
-parser.add_option('--degree_potential',
-                  type='int',
-                  action='store',
-                  default=6)
-parser.add_option('--degree_property',
-                  type='int',
-                  action='store',
-                  default=6)
-parser.add_option('--energy_precision',
-                  type='float',
-                  action='store',
-                  default=1.0e-12)
-parser.add_option('--nr-solutions',
-                  type='int',
-                  action='store',
-                  default=2)
-parser.add_option('--nr-steps',
-                  type='int',
-                  action='store',
-                  default=1000)
-parser.add_option('--to-level',
-                  type='int',
-                  action='store',
-                  default=1)
-
-(options, args) = parser.parse_args()
-
-if len(sys.argv) == 1:
-    # user has given no arguments: print help and exit
-    print parser.format_help().strip()
-    sys.exit()
+    amu_to_au      = 1822.888479031408
+    hartree_to_hz  = 6.579683920721e15
+    hartree_to_cm1 = 2.194746313705e5
 
 
-l = []
+    usage = '''
+      example: ./%prog --file=CHFClBr_mode3_x2c_b3lyp'''
+
+    parser = OptionParser(usage)
+
+    parser.add_option('--file',
+                      type='string',
+                      action='store',
+                      default=None,
+                      help='File to parse',
+                      metavar='FILE')
+    parser.add_option('--degree_potential',
+                      type='int',
+                      action='store',
+                      default=6)
+    parser.add_option('--degree_property',
+                      type='int',
+                      action='store',
+                      default=6)
+    parser.add_option('--energy_precision',
+                      type='float',
+                      action='store',
+                      default=1.0e-12)
+    parser.add_option('--nr-solutions',
+                      type='int',
+                      action='store',
+                      default=2)
+    parser.add_option('--nr-steps',
+                      type='int',
+                      action='store',
+                      default=1000)
+    parser.add_option('--to-level',
+                      type='int',
+                      action='store',
+                      default=1)
+
+    (options, args) = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        # user has given no arguments: print help and exit
+        print parser.format_help().strip()
+        sys.exit()
 
 
+    with open(options.file, 'r') as stream:
+        try:
+            f = yaml.load(stream)
+        except yaml.YAMLError as e:
+            print(e)
 
-with open(options.file, 'r') as stream:
-    try:
-        f = yaml.load(stream)
-    except yaml.YAMLError as e:
-        print(e)
+
+    harmonic_frequency_cm1 = f['harmonic_frequency_cm1']
+    reduced_mass_amu = f['reduced_mass_amu']
+
+    l = []
+    for step in f['steps']:
+        l.append((step['displacement'], step['potential'], step['property']))
+    l.sort()
+
+    q_l = []
+    potential_l = []
+    property_l = []
+    for x in l:
+        q_l.append(x[0])
+        potential_l.append(x[1])
+        property_l.append(x[2])
+
+    potential_x  = numpy.array(q_l[:])
+    potential_y  = numpy.array(potential_l[:])
+    potential_y -= min(potential_y)
+
+    potential_coef = numpy.polyfit(potential_x, potential_y, options.degree_potential)
+
+    property_x = numpy.array(q_l[:])
+    property_y = numpy.array(property_l[:])
+
+    property_coef = numpy.polyfit(property_x, property_y, options.degree_property)
+
+    copy_y = []
+    for y in potential_y:
+        copy_y.append(y)
+    copy_y.sort()
+    fourth_lowest_energy = copy_y[3]
+    h_x = []
+    h_y = []
+    for i in range(len(potential_x)):
+        if potential_y[i] < fourth_lowest_energy:
+            h_x.append(potential_x[i])
+            h_y.append(potential_y[i])
+    potential_coef_harmonic = numpy.polyfit(h_x, h_y, 2)
+
+    print
+    print 'version            =', version
+    print
+    print 'reduced mass       =', reduced_mass_amu
+    print 'harmonic frequency =', harmonic_frequency_cm1
+    print 'degree potential   =', options.degree_potential
+    print 'degree property    =', options.degree_property
+    print 'energy precision   =', options.energy_precision
+    print 'nr solutions       =', options.nr_solutions
+    print 'nr steps           =', options.nr_steps
+
+    transition_frequency_previous = 0.0
+
+    q_max = 0.5
+    while True:
+        q, psi_squared, energy, diff_hz, transition_frequency = run_numerov(options,
+                                                                            reduced_mass_amu,
+                                                                            amu_to_au,
+                                                                            hartree_to_hz,
+                                                                            hartree_to_cm1,
+                                                                            potential_coef,
+                                                                            property_coef,
+                                                                            q_max)
+        if abs(transition_frequency - transition_frequency_previous) < 1.0e-1:
+            break
+        q_max += 0.1
+        transition_frequency_previous = transition_frequency
+
+    # get harmonic frequency from numerov
+    x1, x2, x3, x4, transition_frequency_harmonic = run_numerov(options,
+                                                                reduced_mass_amu,
+                                                                amu_to_au,
+                                                                hartree_to_hz,
+                                                                hartree_to_cm1,
+                                                                potential_coef_harmonic,
+                                                                property_coef,
+                                                                q_max)
+
+    p0        = property_coef[-1]*hartree_to_hz
+    p1        = property_coef[-2]*hartree_to_hz
+    p2        = 2.0*property_coef[-3]*hartree_to_hz
+    v3        = 6.0*potential_coef[-4]
+    mass      = reduced_mass_amu*amu_to_au
+    frequency = harmonic_frequency_cm1/hartree_to_cm1
+
+    n = options.to_level
+    delta_2 = p2
+    delta_3 = delta_2 - p1*v3/(mass*frequency*frequency)
+    delta_2 *= n/(mass*frequency)
+    delta_3 *= n/(mass*frequency)
+
+    print
+    print '%12s %10s %10s %10s %10s %10s %10s %7s %7s %4s' % ('p(0)', 'p(1)', 'p(2)', 'v(3)',
+                                                         'delta(2)', 'delta(3)', 'numerov', 'freq', 'freq h', 'q')
+    print '%12.6f %10.6f %10.6f %10.6f %10.6f %10.6f %10.6f %7.1f %7.1f %4.1f' % (p0, p1, p2, v3,
+                                                                                 delta_2, delta_3,
+                                                                                  diff_hz,
+                                                                                  transition_frequency,
+                                                                                  transition_frequency_harmonic,
+                                                                                  q_max)
+
+    plot_file = string.replace(options.file, '.in', '')
 
 
-harmonic_frequency_cm1 = f['harmonic_frequency_cm1']
-reduced_mass_amu = f['reduced_mass_amu']
-
-l = []
-for step in f['steps']:
-    l.append((step['displacement'], step['potential'], step['property']))
-l.sort()
-
-q_l = []
-potential_l = []
-property_l = []
-for x in l:
-    q_l.append(x[0])
-    potential_l.append(x[1])
-    property_l.append(x[2])
-
-#-------------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------------
-
-potential_x  = numpy.array(q_l[:])
-potential_y  = numpy.array(potential_l[:])
-potential_y -= min(potential_y)
-
-potential_coef = numpy.polyfit(potential_x, potential_y, options.degree_potential)
-
-property_x = numpy.array(q_l[:])
-property_y = numpy.array(property_l[:])
-
-property_coef = numpy.polyfit(property_x, property_y, options.degree_property)
-
-copy_y = []
-for y in potential_y:
-    copy_y.append(y)
-copy_y.sort()
-fourth_lowest_energy = copy_y[3]
-h_x = []
-h_y = []
-for i in range(len(potential_x)):
-    if potential_y[i] < fourth_lowest_energy:
-        h_x.append(potential_x[i])
-        h_y.append(potential_y[i])
-potential_coef_harmonic = numpy.polyfit(h_x, h_y, 2)
-
-#-------------------------------------------------------------------------------
-
-print
-print 'version            =', version
-print
-print 'reduced mass       =', reduced_mass_amu
-print 'harmonic frequency =', harmonic_frequency_cm1
-print 'degree potential   =', options.degree_potential
-print 'degree property    =', options.degree_property
-print 'energy precision   =', options.energy_precision
-print 'nr solutions       =', options.nr_solutions
-print 'nr steps           =', options.nr_steps
-
-transition_frequency_previous = 0.0
-
-q_max = 0.5
-while True:
-    q, psi_squared, energy, diff_hz, transition_frequency = run_numerov(potential_coef, property_coef, q_max)
-    if abs(transition_frequency - transition_frequency_previous) < 1.0e-1:
-        break
-    q_max += 0.1
-    transition_frequency_previous = transition_frequency
-
-# get harmonic frequency from numerov
-x1, x2, x3, x4, transition_frequency_harmonic = run_numerov(potential_coef_harmonic, property_coef, q_max)
-
-p0        = property_coef[-1]*hartree_to_hz
-p1        = property_coef[-2]*hartree_to_hz
-p2        = 2.0*property_coef[-3]*hartree_to_hz
-v3        = 6.0*potential_coef[-4]
-mass      = reduced_mass_amu*amu_to_au
-frequency = harmonic_frequency_cm1/hartree_to_cm1
-
-n = options.to_level
-delta_2 = p2
-delta_3 = delta_2 - p1*v3/(mass*frequency*frequency)
-delta_2 *= n/(mass*frequency)
-delta_3 *= n/(mass*frequency)
-
-print
-print '%12s %10s %10s %10s %10s %10s %10s %7s %7s %4s' % ('p(0)', 'p(1)', 'p(2)', 'v(3)',
-                                                     'delta(2)', 'delta(3)', 'numerov', 'freq', 'freq h', 'q')
-print '%12.6f %10.6f %10.6f %10.6f %10.6f %10.6f %10.6f %7.1f %7.1f %4.1f' % (p0, p1, p2, v3,
-                                                                             delta_2, delta_3,
-                                                                              diff_hz,
-                                                                              transition_frequency,
-                                                                              transition_frequency_harmonic,
-                                                                              q_max)
-
-plot_file = string.replace(options.file, '.in', '')
+if __name__ == '__main__':
+    main()
