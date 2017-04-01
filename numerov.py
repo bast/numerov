cacdcc4427b7d67d18e6f48478eaec54129c3efe
Radovan Bast <bast@users.noreply.github.com>
2017-04-01 21:41:37 +0200

    mv imports

--- numerov.py
+++ numerov.py
@@ -1,19 +1,17 @@
+import yaml
+import sys
+import string
+import os
+import numpy
+from optparse import OptionParser
+
+
 version = '1.1.0'
 
 amu_to_au      = 1822.888479031408
 hartree_to_hz  = 6.579683920721e15
 hartree_to_cm1 = 2.194746313705e5
 
-
-import yaml
-import sys
-import string
-import os
-import numpy
-
-from optparse import OptionParser
-
-#-------------------------------------------------------------------------------
 
 usage = '''
   example: ./%prog --file=CHFClBr_mode3_x2c_b3lyp'''
