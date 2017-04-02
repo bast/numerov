3ec79da79c6f6dd50ec75125c2619f80edaef7b6
Radovan Bast <bast@users.noreply.github.com>
2017-04-02 21:01:54 +0200

    reorganize

--- _numerov/constants.py
+++ _numerov/constants.py
@@ -1,3 +1,10 @@
+# Copyright 2017 Radovan Bast. All rights reserved.
+# Use of this source code is governed by a the Mozilla Public License v2.0 that
+# can be found in the LICENSE file. If a copy of the license was not
+# distributed with this file, you can obtain one at
+# http://mozilla.org/MPL/2.0/.
+
+
 constants = {'amu_to_au': 1822.888479031408,
              'hartree_to_hz': 6.579683920721e15,
              'hartree_to_cm1': 2.194746313705e5}
