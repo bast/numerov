e08a56a9b410fad6c3aaf1554e226bc0662ad8d0
Radovan Bast <bast@users.noreply.github.com>
2017-04-02 11:13:04 +0200

    add license header

--- numerov.py
+++ numerov.py
@@ -1,3 +1,10 @@
+# Copyright 2017 Radovan Bast. All rights reserved.
+# Use of this source code is governed by a the Mozilla Public License v2.0 that
+# can be found in the LICENSE file. If a copy of the license was not
+# distributed with this file, you can obtain one at
+# http://mozilla.org/MPL/2.0/.
+
+
 import yaml
 import sys
 import numpy
