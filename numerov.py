acfb88bbe8ca43b9da946100e5b15b223dc6bd1e
Radovan Bast <bast@users.noreply.github.com>
2017-04-02 13:31:08 +0200

    implement unit test for run_numerov

--- numerov.py
+++ numerov.py
@@ -10,14 +10,13 @@ import sys
 import numpy
 
 
-def run_numerov(constants,
-                pot_energy_coefs,
+def run_numerov(pot_energy_coefs,
                 exp_value_coefs,
                 displacement_range,
                 num_steps,
                 num_solutions,
                 energy_precision,
-                reduced_mass_amu):
+                reduced_mass_au):
 
     n = num_steps + 1
     step = (displacement_range[1] - displacement_range[0]) / num_steps
@@ -29,7 +28,8 @@ def run_numerov(constants,
     g = numpy.zeros(n)
     psi = numpy.zeros(n)
     psi_squared = numpy.zeros([num_solutions, n])
-    energy = numpy.zeros(num_solutions)
+    energies_hartree = numpy.zeros(num_solutions)
+    averaged_exp_values_au = numpy.zeros(num_solutions)
 
     for i in range(n):
         q[i] = displacement_range[0] + i * step
@@ -40,12 +40,9 @@ def run_numerov(constants,
     energy_step = 1e-4
     num_nodes_last = 0
 
-    expectation_value_reference = 0.0
-    zero_point_energy = 0.0
-
     while num_nodes_last < num_solutions:
 
-        g = reduced_mass_amu * constants['amu_to_au'] * 2.0 * (pot_energies - energy_guess)
+        g = reduced_mass_au * 2.0 * (pot_energies - energy_guess)
 
         psi[0] = 0.0
         psi[1] = 1.0e-6
@@ -89,19 +86,9 @@ def run_numerov(constants,
                     i_right = i
                     done = 1
 
-            expectation_value = 2.0 * numpy.dot(psi, exp_values * psi)
             psi_squared[num_nodes - 1] = psi**2.0
-
-            energy[num_nodes - 1] = energy_guess
-
-            if num_nodes == 1:
-                expectation_value_reference = expectation_value
-                zero_point_energy = energy_guess
-
-            transition_frequency = (energy_guess - zero_point_energy) * constants['hartree_to_cm1']
-
-            diff_au = (expectation_value - expectation_value_reference)
-            diff_hz = diff_au * constants['hartree_to_hz']
+            energies_hartree[num_nodes - 1] = energy_guess
+            averaged_exp_values_au[num_nodes - 1] = numpy.dot(psi, exp_values * psi)
 
             energy_step = 1.0e-4
             num_nodes_last = num_nodes_last + 1
@@ -115,7 +102,7 @@ def run_numerov(constants,
 
         energy_guess = energy_guess + energy_step
 
-    return q, psi_squared, energy, diff_hz, transition_frequency
+    return q, psi_squared, energies_hartree, averaged_exp_values_au
 
 
 def main():
@@ -171,30 +158,36 @@ def main():
     print('nr solutions       =', input_data['num_solutions'])
     print('nr steps           =', input_data['num_steps'])
 
-    transition_frequency = sys.float_info.max
-    transition_frequency_previous = -sys.float_info.max
+    transition_frequency_previous = sys.float_info.max
     displacement_range = (-0.5, 0.5)
-    while abs(transition_frequency - transition_frequency_previous) > 1.0e-1:
-        q, psi_squared, energy, diff_hz, transition_frequency = run_numerov(constants,
-                                                                            pot_energy_coefs,
-                                                                            exp_value_coefs,
-                                                                            displacement_range,
-                                                                            input_data['num_steps'],
-                                                                            input_data['num_solutions'],
-                                                                            input_data['energy_precision'],
-                                                                            input_data['reduced_mass_amu'])
+    while True:
+        q, psi_squared, energies_hartree, averaged_exp_values_au = run_numerov(pot_energy_coefs,
+                                                                               exp_value_coefs,
+                                                                               displacement_range,
+                                                                               input_data['num_steps'],
+                                                                               input_data['num_solutions'],
+                                                                               input_data['energy_precision'],
+                                                                               input_data['reduced_mass_amu'] * constants['amu_to_au'])
+        transition_frequency = (energies_hartree[input_data['num_solutions'] - 1] - energies_hartree[0]) * constants['hartree_to_cm1']
+        if abs(transition_frequency - transition_frequency_previous) < 1.0e-1:
+            break
         displacement_range = (displacement_range[0] - 0.1, displacement_range[1] + 0.1)
         transition_frequency_previous = transition_frequency
 
+    # factor two is hardcoded for PNC difference
+    diff_au = 2.0*(averaged_exp_values_au[input_data['num_solutions'] - 1] - averaged_exp_values_au[0])
+    diff_hz = diff_au * constants['hartree_to_hz']
+
     # get harmonic frequency from numerov
-    _, _, _, _, transition_frequency_harmonic = run_numerov(constants,
-                                                            pot_energy_coefs_harmonic,
-                                                            exp_value_coefs,
-                                                            displacement_range,
-                                                            input_data['num_steps'],
-                                                            input_data['num_solutions'],
-                                                            input_data['energy_precision'],
-                                                            input_data['reduced_mass_amu'])
+    _, _, energies_hartree, _ = run_numerov(pot_energy_coefs_harmonic,
+                                            exp_value_coefs,
+                                            displacement_range,
+                                            input_data['num_steps'],
+                                            input_data['num_solutions'],
+                                            input_data['energy_precision'],
+                                            input_data['reduced_mass_amu'] * constants['amu_to_au'])
+    transition_frequency_harmonic = (energies_hartree[input_data['num_solutions'] - 1] - energies_hartree[0]) * constants['hartree_to_cm1']
+
     p0 = exp_value_coefs[-1] * constants['hartree_to_hz']
     p1 = exp_value_coefs[-2] * constants['hartree_to_hz']
     p2 = exp_value_coefs[-3] * constants['hartree_to_hz'] * 2.0
@@ -221,6 +214,59 @@ def main():
 
     plot_file = input_file.replace('.in', '')
 
+
+def test_run_numerov():
+    pot_energy_coefs = numpy.array([-2.45560869e-01,
+                                    -8.88252151e-03,
+                                     1.24439946e-01,
+                                     1.93259856e-01,
+                                     2.78860663e-01,
+                                    -5.62738650e-05,
+                                    -5.78784571e-08])
+    exp_value_coefs = numpy.array([-1.45680171e-14,
+                                   -2.78094078e-16,
+                                    1.62725432e-15,
+                                   -1.99732822e-15,
+                                    3.08772558e-15,
+                                    2.06211298e-15,
+                                   -7.30656049e-15])
+    displacement_range = (-0.5, 0.5)
+    num_steps = 21
+    num_solutions = 3
+    energy_precision = 1e-12
+    reduced_mass_au = 26245.03
+    q, psi_squared, energies_hartree, averaged_exp_values_au = run_numerov(pot_energy_coefs,
+                                                                           exp_value_coefs,
+                                                                           displacement_range,
+                                                                           num_steps,
+                                                                           num_solutions,
+                                                                           energy_precision,
+                                                                           reduced_mass_au)
+
+    q_ref = [-0.5, -0.45238095238095238, -0.40476190476190477,
+             -0.35714285714285715, -0.30952380952380953, -0.26190476190476192,
+             -0.2142857142857143, -0.16666666666666669, -0.11904761904761907,
+             -0.071428571428571452, -0.023809523809523836, 0.023809523809523725,
+             0.071428571428571397, 0.11904761904761907, 0.16666666666666663,
+             0.21428571428571419, 0.26190476190476186, 0.30952380952380953,
+             0.3571428571428571, 0.40476190476190466, 0.45238095238095233, 0.5]
+    assert numpy.allclose(q, q_ref)
+
+    psi_0_squared_ref = [0.0, 5.8115604540099223e-11, 4.5928270821175377e-09,
+                         2.2581144389236216e-07, 7.0160833333126289e-06, 0.00013741125594800236,
+                         0.0016860686764846274, 0.012837351969918346, 0.059868399265284117,
+                         0.16833949851844193, 0.28036406815649312, 0.27146736153858203,
+                         0.15004107659776397, 0.046521109971706026, 0.0079563591949426953,
+                         0.00073700740789436057, 3.6124036211418563e-05, 9.0535270296150204e-07,
+                         1.0972855205998383e-08, 4.808188792726663e-11, 4.9096949798023428e-10, 0.0]
+
+    assert numpy.allclose(psi_squared[0], psi_0_squared_ref)
+
+    energies_hartree_ref = [0.0023035871039000012, 0.0068957376078000003, 0.011460087763699986]
+    assert numpy.allclose(energies_hartree, energies_hartree_ref)
+
+    averaged_exp_values_au_ref = [-7.3021260330311477e-15, -7.2926761553595848e-15, -7.2818315936417763e-15]
+    assert numpy.allclose(averaged_exp_values_au, averaged_exp_values_au_ref, rtol=1e-05, atol=1e-30)
 
 if __name__ == '__main__':
     main()
