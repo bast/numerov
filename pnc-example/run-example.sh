ca6d7916d53bcadda3dc787a416729279e324817
Radovan Bast <bast@users.noreply.github.com>
2017-07-21 17:07:26 +0200

    wrap long lines for easier reading

--- pnc-example/run-example.sh
+++ pnc-example/run-example.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-python ../numerov.py --general=general.yml --force-field=force.yml --exp-values=property.yml > numerov-output.yml
+python ../numerov.py --general=general.yml \
+                     --force-field=force.yml \
+                     --exp-values=property.yml > numerov-output.yml
+
 python pnc.py numerov-output.yml > pnc-output.yml
-python plot.py --force-field=force.yml --exp-values=property.yml --numerov-output=numerov-output.yml --img=plot.png
+
+python plot.py --force-field=force.yml \
+               --exp-values=property.yml \
+               --numerov-output=numerov-output.yml \
+               --img=plot.png
 
 exit 0
